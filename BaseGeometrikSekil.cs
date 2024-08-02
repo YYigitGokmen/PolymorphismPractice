56fcebe1ccc573f235a21056e184e14bc171ab4b
Yılmaz Yiğit Gökmen <gokmenyyigit@gmail.com>
2024-08-02 19:22:14 +0300

    Add files via upload

---
+++ BaseGeometrikSekil.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismPractice
+{
+    public abstract class BaseGeometrikSekil
+    {
+
+        public double Witdh {  get; set; }
+        public double Height {  get; set; }
+
+        public abstract double AlanHesapla();
+        
+            
+
+
+    }
+
+    //Classlarımızı ortak bir çatı altında toplayarak BaseGeometrikSekil classından türetebiliriz.
+    public class Square : BaseGeometrikSekil 
+    { 
+
+        public Square(double witdh,double height) 
+
+        {
+            Witdh = witdh;
+            Height = height;
+        
+        }
+
+
+        public override double AlanHesapla()
+        {
+            return Witdh * Height;
+        }
+
+    }
+
+    public class Rectangle : BaseGeometrikSekil
+    {
+        public Rectangle(double witdh, double height)
+
+        {
+            Witdh = witdh;
+            Height = height;
+
+        }
+
+        public override double AlanHesapla()
+        {
+            return Witdh * Height;
+
+        }
+
+
+    }
+
+    public class RightTriangle : BaseGeometrikSekil
+    {
+
+        public RightTriangle(double witdh, double height)
+
+        {
+            Witdh = witdh;
+            Height = height;
+
+        }
+
+        public override double AlanHesapla()
+        {
+            return (Witdh * Height)/2;
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+using PolymorphismPractice;
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+
+
+        Square kare = new Square(3,5);
+
+
+        Rectangle dikdörtgen = new Rectangle(10, 20);
+
+
+        RightTriangle diküçgen = new RightTriangle(5, 30);
+
+
+        Console.WriteLine($"Karenin alanı {kare.AlanHesapla()}");
+        Console.WriteLine($"DikDörtgenin alanı {dikdörtgen.AlanHesapla()}");
+        Console.WriteLine($"Diküçgenin alanı {diküçgen.AlanHesapla()}");
+
+
+      
+
+    }
+}
+
+
